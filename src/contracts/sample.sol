1acca474747c28867a42a785810ffed0f74fd2b1
Mike Seese <seesemichaelj@gmail.com>
2018-02-21 06:45:51 -0500

    add some stuff to help test variables

--- src/contracts/sample.sol
+++ src/contracts/sample.sol
@@ -38,7 +38,26 @@ contract Sample {
         return v;
     }
 
+    struct s {
+        uint a;
+    }
+
+    struct c {
+        uint a;
+    }
+
     function test5() public {
+        uint8[5] memory crazy;
+        crazy[0] = 1;
+        crazy[1] = 2;
+        crazy[2] = 3;
+        crazy[3] = 4;
+        crazy[4] = 5;
+        s memory wtf;
+        s memory wtf2;
+        c memory wtf3;
+        wtf.a = 0;
+        wtf2.a = 10;
         uint256 newVal = 108;
         uint256 nextVal = newVal / 2;
         newVal += 1;
@@ -49,5 +68,19 @@ contract Sample {
     function test5a(uint _val) private pure returns (uint) {
         uint nextVal = _val * 2;
         return nextVal;
+    }
+
+    function test6() public {
+        bool t1;
+        int8 t2;
+        address t3;
+        byte t4;
+        bytes32 t5;
+        t1 = true;
+        t2 = -1;
+        t3 = msg.sender;
+        t4 = byte(2);
+        t5 = "hello";
+        t1 = false; // for breakpoint purposes
     }
 }
